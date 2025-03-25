a2f9290d9b50c71519a1a4a79ee3f62fc1a1ca50
gyoungsubLee <dltjq2323@gmail.com>
2025-03-25 15:45:24 +0900

    [ 싱글톤 패턴 ] 구현 2: synchronized

---
+++ src/singleton/v2/Settings.java
@@ -0,0 +1,19 @@
+package singleton.v2;
+
+/**
+  구현) synchronized
+    * 매번 동기화 연산(synchronized)이 발생되어 성능에 약간 불이득이 있다.
+    * 락을 통한 제어 비용 발생
+*/
+public class Settings {
+    private static Settings instance;
+
+    private Settings() { }
+
+    public static synchronized Settings getInstance() {
+        if (instance == null) {
+            instance = new Settings();
+        }
+        return instance;
+    }
+}
