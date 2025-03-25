7310d671395f00868dacd022f76a263f79b0e8a7
gyoungsubLee <dltjq2323@gmail.com>
2025-03-25 17:10:01 +0900

    [ 싱글톤 패턴 ] 싱글톤 패턴이 깨지는 상황: 역직렬화

---
+++ src/singleton/v5/deserialization/App.java
@@ -0,0 +1,17 @@
+package singleton.v5.deserialization;
+
+import java.io.*;
+
+public class App {
+        public static void main(String[] args) throws IOException, ClassNotFoundException {
+            Settings settings = Settings.getInstance();
+            Settings settings1 = null;
+            try (ObjectOutput out = new ObjectOutputStream(new FileOutputStream("settings.obj"))) {
+                out.writeObject(settings);
+            }
+            try (ObjectInput in = new ObjectInputStream(new FileInputStream("settings.obj"))) {
+                settings1 = (Settings) in.readObject();
+            }
+            System.out.println(settings == settings1); // false
+        }
+}
---
+++ src/singleton/v5/deserialization/Settings.java
@@ -0,0 +1,19 @@
+package singleton.v5.deserialization;
+
+import java.io.Serializable;
+
+public class Settings implements Serializable {
+    // Serializable ID 생략..
+
+    private Settings() {}
+
+    private static class SettingsHolder {
+        private static final Settings INSTANCE = new Settings();
+    }
+
+    public static Settings getInstance() {
+        return SettingsHolder.INSTANCE;
+    }
+}
+
+
