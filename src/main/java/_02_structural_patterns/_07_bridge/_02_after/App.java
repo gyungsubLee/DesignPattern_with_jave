a7b1fab856d71ffcddc27f62855e471d05d7cd16
gyoungsubLee <dltjq2323@gmail.com>
2025-03-30 21:04:15 +0900

    [ 브릿지 패턴 ] 적용 예제 생략 코드 추가

--- src/main/java/_02_structural_patterns/_07_bridge/_02_after/App.java
+++ src/main/java/_02_structural_patterns/_07_bridge/_02_after/App.java
@@ -1,4 +1,19 @@
 package _02_structural_patterns._07_bridge._02_after;
 
-public class App {
+import _02_structural_patterns._07_bridge._02_after.champion.Champion;
+import _02_structural_patterns._07_bridge._02_after.champion.아리;
+import _02_structural_patterns._07_bridge._02_after.skin.KDA;
+import _02_structural_patterns._07_bridge._02_after.skin.PoolParty;
+
+public abstract class App implements Champion {
+
+    public static void main(String[] args) {
+        Champion kda아리 = new 아리(new KDA());
+        kda아리.skillQ();
+        kda아리.skillW();
+
+        Champion poolParty아리 = new 아리(new PoolParty());
+        poolParty아리.skillR();
+        poolParty아리.skillW();
+    }
 }
