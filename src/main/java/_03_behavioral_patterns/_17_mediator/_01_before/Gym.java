01e6eb1a02a51051f21f564480cb787e958d2093
gyoungsubLee <dltjq2323@gmail.com>
2025-04-20 20:33:25 +0900

    [ 중재자 패턴 ] 기본 예제

---
+++ src/main/java/_03_behavioral_patterns/_17_mediator/_01_before/Gym.java
@@ -0,0 +1,10 @@
+package _03_behavioral_patterns._17_mediator._01_before;
+
+public class Gym {
+
+    private CleaningService cleaningService = new CleaningService();
+
+    public void clean() {
+        cleaningService.clean(this);
+    }
+}
